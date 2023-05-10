159cb93b860a1b4d72f1b797fdcca1d87eed4d63
daqiangli <dqli161@163.com>
2023-05-10 18:16:34 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,23 +1,45 @@
-vim.g.mapleader = " "
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true}
+local os = vim.loop.os_uname()
 
-local keymap = vim.keymap
+-- move
+map("n", "J", "5j", opt)
+map("n", "K", "5k", opt)
+map("n", "H", "^", opt)
+map("n", "L", "g_", opt)
+
+-- window
+map("n", "sv", ":vsp<CR>", opt)   -- split vertical
+map("n", "sh", ":sp<CR>",  opt)   -- split horizontal
+map("n", "sc", "<C-w>c",   opt)   -- close current window
+map("n", "so", "<C-w>o",   opt)   -- close other windows
+-- jump between windows
+map("n", "<leader>h", "<C-w>h", opt)
+map("n", "<leader>j", "<C-w>j", opt)
+map("n", "<leader>k", "<C-w>k", opt)
+map("n", "<leader>l", "<C-w>l", opt)
+-- jump between buffer
+map("n", "<C-l>", ":bnext<CR>", opt)
+map("n", "<C-h>", ":bprevious<CR>", opt)
 
 
----- insert mode  ------
-keymap.set("i", "jk", "<ESC>")
+-- move line of text using ALT+[jk]
+map("n", "<M-k>", "mz:m-2<cr>`z", opt)
+map("n", "<M-j>", "mz:m+<cr>`z",  opt)
+map("v", "<M-j>", ":m'>+<cr>`<my`>mzgv`yo`z", opt)
+map("v", "<M-k>", ":m'<-2<cr>`>my`<mzgv`yo`z", opt)
 
+-- quick quit & save
+map("i", "<leader>q",  "<ESC>:q<CR>",  opt)
+map("i", "<leader>wq", "<ESC>:wq<CR>", opt)
+map("i", "<leader>w",  "<ESC>:w<CR>",  opt)
 
----- visual mode -------
--- move multiple line
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-
----- normal mode ------
--- add window in vertical direction
-keymap.set("n", "<leader>sv", "<C-w>v")
--- add window in horizontal direction
-keymap.set("n", "<leader>sh", "<C-w>s")
-
--- cancel highlight
-keymap.set("n","<leader>nh", ":nohl<CR>")
+-- move to the start or end of line
+map("i", "<C-a>", "<ESC>I", opt)
+map("i", "<C-e>", "<ESC>A", opt)
+map("i", "<C-o>", "<ESC>o", opt)
+map("i", "<C-h>", "<Left>", opt)
+map("i", "<C-j>", "<Down>", opt)
+map("i", "<C-k>", "<Up>",   opt)
+map("i", "<C-l>", "<Right>",opt)
+map("i", "<C-d>", "<Delete>",opt)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,29 +1,37 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- This file is automatically loaded by plugins.config
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
+
 local opt = vim.opt
 
--- highlight current line
+-- Enable highlighting of the current line
 opt.cursorline = true
--- highlight current column
--- opt.cursorcolumn = true
 
+-- char display
+opt.list = true
+opt.listchars="tab:>-,trail:-,nbsp:+,extends:→,precedes:←"
 
--- intent
+-- indent
 opt.tabstop = 4
+opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.autoindent = true
--- opt.expandtab = true
+opt.smartindent = true
 
-
--- clipboard
--- opt.clipboard:append("unnamedplus")
+-- spell
+opt.spelllang = {"en"}
 
 -- window
---
+opt.splitbelow = true
+opt.splitright = true
 
+-- 右侧参考线,超过表示代码太长了
+-- opt.colorcolumn = "80"
 
--- search
---opt.ignorecase = true
---opt.smartcase = true
-
--- appearance
--- opt.termguicolors = true
--- opt.signcolumn = "yes"
+-- encoding
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
