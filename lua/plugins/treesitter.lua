73ca72f8645a5c99389f486a4a2a6691bcc30015
daqiangli <dqli161@163.com>
2023-06-18 17:58:42 +0800

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,29 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require('nvim-treesitter.configs').setup {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require('nvim-treesitter.configs').setup{
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
-				ensure_installed = { "lua", "vim", "vimdoc", "query", "make", "json", "comment", "gitignore" },
-
+				ensure_installed = {"lua"},
 				-- Install parsers synchronously (only applied to `ensure_installed`)
 				sync_install = true,
-
 				-- Automatically install missing parsers when entering buffer
 				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 				auto_install = true,
-				
-                highlight = {
-                  enable = false,
-                  -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                  -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                  -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                  -- Instead of true it can also be a list of languages
-                  additional_vim_regex_highlighting = false,
-                },
-                indent = {
-                  enable = true,
-                }
-            }
-        end,
-    }
+				highlight = {
+					enable = false,
+					-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+					-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+					-- the name of the parser)
+					-- list of language that will be disabled
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = false,
+				}
+			}
+		end,
+	}
 }
