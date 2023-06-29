50f3d9ee76ad57c944cdaba73d16acfa8dee33a9
daqiangli <dqli161@163.com>
2023-06-29 16:02:26 +0800

    set auto install

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,7 +10,20 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "gopls" },
+                -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+                -- This setting has no relation with the `ensure_installed` setting.
+                -- Can either be:
+                --   - false: Servers are not automatically installed.
+                --   - true: All servers set up via lspconfig are automatically installed.
+                --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+                --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+                ---@type boolean
+                automatic_installation = true,
+
+                -- See `:h mason-lspconfig.setup_handlers()`
+                ---@type table<string, fun(server_name: string)>?
+                handlers = nil,
             })
 
             local lspconfig = require('lspconfig')
