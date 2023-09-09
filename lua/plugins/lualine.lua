c549fccbbd3f23b9048164fa37c77a4d4c3dd396
lidaqiang <dqli161@163.com>
2023-09-09 22:17:59 +0800

    disable icon

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,9 @@ return {
         require("lualine").setup({
             options = {
                 icons_enabled = false,
-                theme = 'auto',
+                theme = 'gruvbox',
+                component_separators = { left = '|', right = '|'},
+                section_separators = { left = '', right = ''},
             }
         })
     end
