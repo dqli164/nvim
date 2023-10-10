c5c8c4cda0f00d92f6ba4eac3b18b734a1a608f8
lidaqiang <dqli161@163.com>
2023-10-10 08:32:16 +0000

    update

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,20 +1,21 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     config = function()
-        require("indent_blankline").setup {
+        require("ibl").setup {
             enabled = true,
-            space_char_blankline = " ",
-            show_trailing_blankline_indent = false,
-            use_treesitter_scope = false,
-            char_highlight_list = {
-                "IndentBlanklineIndent1",
-            },
-            space_char_highlight_list = {
-                "IndentBlanklineIndent1",
-            },
+            -- space_char_blankline = " ",
+            -- show_trailing_blankline_indent = false,
+            -- use_treesitter_scope = false,
+            -- char_highlight_list = {
+            --     "IndentBlanklineIndent1",
+            -- },
+            -- space_char_highlight_list = {
+            --     "IndentBlanklineIndent1",
+            -- },
             -- context_patterns = { 'class', 'function', 'method'},
-            show_current_context = true,
-            show_current_context_start = true
+            -- show_current_context = true,
+            -- show_current_context_start = true
         }
     end
 }
