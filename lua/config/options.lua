e1bfddae74a45cd613f7ccad34d81361f50b8817
lidaqiang <dqli161@163.com>
2023-10-13 02:30:15 +0000

    comment indent-blankline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@ opt.clipboard:prepend("unnamedplus")
 opt.completeopt = {'menu', 'menuone', 'noselect'}
 opt.mouse = 'a'
 opt.number = true
--- opt.relativenumber = true
+opt.relativenumber = true
 
 -- enable highlighting of current line
 opt.cursorline = true
@@ -20,14 +20,14 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.autoindent = true
-opt.smartindent = true
-opt.expandtab = true
-opt.smarttab = true
+-- opt.smartindent = true
+opt.expandtab = false
+-- opt.smarttab = true
 
 -- char display
 opt.list = true
 -- opt.listchars:append "tab:>-,trail:-,nbsp:.,extends:>,precedes:<,space:."
--- opt.listchars:append "tab:>-,trail:-,nbsp:.,extends:>,precedes:<"
+opt.listchars:append "tab:>-,trail:-,nbsp:.,extends:>,precedes:<"
 
 -- highlight
 opt.hlsearch = true
@@ -38,9 +38,9 @@ opt.background = "dark"
 vim.cmd([[colorscheme  gruvbox]])
 
 -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#363636 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#4F4F4F gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#696969 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineContextChar guifg=#B5B5B5 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#4F4F4F gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#696969 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineContextChar guifg=#B5B5B5 gui=nocombine]]
 
 -- spell
 opt.spelllang = { "en" }
