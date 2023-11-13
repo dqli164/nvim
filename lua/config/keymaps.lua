2a64bf0f08ea8b2dc982de6b2f7f6a81ec44e66b
lidaqiang <dqli161@163.com>
2023-11-13 16:01:42 +0800

    change line

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,8 @@ local opt = {noremap = true, silent = true}
 local map = vim.api.nvim_set_keymap
 
 -- move more quickly
-map("n", "J", "9j", opt)
-map("n", "K", "9k", opt)
+map("n", "J", "12j", opt)
+map("n", "K", "12k", opt)
 map("n", "H", "^", opt)
 map("n", "L", "g_", opt)
 
