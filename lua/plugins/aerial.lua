07167df6c3a7c6d28f21657d9fa2c7309b2b71f8
lidaqiang <dqli161@163.com>
2024-01-10 02:16:57 +0000

    comment

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,3 +1,4 @@
+-- Neovim plugin for a code outline window
 return {
     'stevearc/aerial.nvim',
     opts = {},
@@ -11,8 +12,8 @@ return {
             -- optionally use on_attach to set keymaps when aerial has attached to a buffer
             on_attach = function(bufnr)
                 -- Jump forwards/backwards with '{' and '}'
-                vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-                vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+                -- vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                -- vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
             end,
         })
         -- You probably also want to set a keymap to toggle aerial
