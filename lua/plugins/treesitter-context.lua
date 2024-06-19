7bedab79f4bee4c95ffb6ec3be97c1d368629985
lidaqiang <dqli161@163.com>
2024-06-19 04:19:33 +0000

    add plugin

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,20 @@
+return {
+    "romgrk/nvim-treesitter-context",
+    lazy = true,
+    event = { "User FileOpened" },
+    config = function()
+        require("treesitter-context").setup({
+            enable = true,
+            throttle = true,
+            max_lines = 0,
+            patterns = {
+                default = {
+                    "class",
+                    "function",
+                    "method",
+                    "func",
+                },
+            },
+        })
+    end,
+}
