f4febf3217a49d9136fc8a00ef853bd1e71fe3be
lidaqiang <dqli161@163.com>
2024-06-19 06:35:34 +0000

    update

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,13 @@
+return {
+   "phaazon/hop.nvim",
+    lazy = true,
+    keys = { "r" },
+    config = function()
+        require("hop").setup({})
+        vim.api.nvim_set_keymap("n", "R", "<cmd>HopChar2<cr>", { silent = true })
+        vim.api.nvim_set_keymap("n", "r", "<cmd>HopChar1<cr>", { silent = true })
+        vim.api.nvim_set_keymap("n", "U", "<cmd>HopWord<cr>", { silent = true })
+        vim.api.nvim_set_keymap("n", "C", "<cmd>HopLine<cr>", { silent = true })
+        vim.api.nvim_set_keymap("n", "P", "<cmd>HopPattern<cr>", { silent = true })
+    end, 
+}
