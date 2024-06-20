bf5f3b4e7a557e54e34e21738bbffc23fad30788
lidaqiang <dqli161@163.com>
2024-06-20 06:28:50 +0000

    update

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,13 +1,25 @@
 return {
-    "phaazon/hop.nvim",
-    lazy = true,
-    keys = { "r" },
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
     config = function()
-        require("hop").setup({})
-        vim.api.nvim_set_keymap("n", "R", "<cmd>HopChar2<cr>", { silent = true })
-        vim.api.nvim_set_keymap("n", "r", "<cmd>HopChar1<cr>", { silent = true })
-        vim.api.nvim_set_keymap("n", "U", "<cmd>HopWord<cr>", { silent = true })
-        vim.api.nvim_set_keymap("n", "C", "<cmd>HopLine<cr>", { silent = true })
-        vim.api.nvim_set_keymap("n", "P", "<cmd>HopPattern<cr>", { silent = true })
-    end,
+        -- you can configure Hop the way you like here; see :h hop-config
+        require'hop'.setup { 
+            keys = 'etovxqpdygfblzhckisuran',
+        }
+        -- place this in one of your configuration file(s)
+        -- local hop = require('hop')
+        -- local directions = require('hop.hint').HintDirection
+        -- vim.keymap.set('', 'f', function()
+        --     hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+        -- end, {remap=true})
+        -- vim.keymap.set('', 'F', function()
+        --     hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+        -- end, {remap=true})
+        -- vim.keymap.set('', 't', function()
+        --     hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+        -- end, {remap=true})
+        -- vim.keymap.set('', 'T', function()
+        --     hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+        -- end, {remap=true})
+    end
 }
